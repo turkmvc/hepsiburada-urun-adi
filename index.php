c7bb0216e31ffd3e6d60af8fe30d328efea6fab7
İbrahim Ş. Örencik <o@yedincisenol.com>
2017-12-14 18:21:32 +0300

    Hata gidermeler

--- index.php
+++ index.php
@@ -5,23 +5,35 @@ require_once "vendor/autoload.php";
 use Symfony\Component\DomCrawler\Crawler;
 use GuzzleHttp\Client;
 
+function sendResponse($code, $responseArray) {
+    http_response_code($code);
+    header('Content-Type: application/json');
+    echo json_encode($responseArray);
+    exit();
+};
+
 $hepsiburadaSku = $_GET['sku'];
+
 if (!$hepsiburadaSku) {
     sendResponse(422, [
-        'message' => 'sku Parametresi ile hepsiburada ürün Sku (stok kodu) gönderilmeli'
+        'message' => 'sku Parametresi ile hepsiburada ürün Sku (stok kodu) gönderilmeli. ?sku=HRSENS-70201 gibi'
     ]);
 }
 
-$httpClient = new Client();
+$httpClient = new Client([
+    'exceptions' => false
+]);
+
 $response   = $httpClient->get('http://www.hepsiburada.com/as-p-' . $hepsiburadaSku);
-$crawler = new Crawler();
+$crawler = new Crawler((string) $response->getBody());
 
 if ($response->getStatusCode() == 200) {
     $name = $crawler->filter('h1.product-name')->text();
     
     sendResponse(200, [
-        'name'  =>  $name
+        'name'  =>  trim($name)
     ]);
+
 } elseif ($response->getStatusCode() == 404) {
     sendResponse(404, [
         'message' => 'Ürün bulunamadı'
@@ -31,10 +43,3 @@ if ($response->getStatusCode() == 200) {
         'message' => 'Bilinmeyen bir hata oluştu'
     ]);
 }
-
-function sendResponse($code, $responseArray) {
-    http_response_code($code);
-    header('Content-Type: application/json');
-    echo json_encode($responseArray);
-    exit();
-};
